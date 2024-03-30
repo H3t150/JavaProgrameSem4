ad57d0f5933a8d409cdebf3cca1b5ab1f59e886b
H3t150 <155147329+H3t150@users.noreply.github.com>
2024-03-30 20:11:15 +0530

    Update P30_Wraper.java

--- Lab/P30_Wraper.java
+++ Lab/P30_Wraper.java
@@ -1,14 +1,28 @@
-package Lab;
-public class P30_Wraper {
+public class WrapperExample {
     public static void main(String[] args) {
-        // Autoboxing: Converting a primitive value into an object of the corresponding wrapper class
+        // 1. Integer.valueOf() - Returns an Integer object representing the specified int value
         int num = 10;
-        Integer numObject = num;  // Autoboxing
-        System.out.println("Value of numObject: " + numObject);
+        Integer integerObj = Integer.valueOf(num);
+        System.out.println("Integer value: " + integerObj);
 
-        // Unboxing: Converting an object of a wrapper class to its corresponding primitive value
-        Integer anotherNumObject = new Integer(20);
-        int anotherNum = anotherNumObject;  // Unboxing
-        System.out.println("Value of anotherNum: " + anotherNum);
+        // 2. Double.parseDouble() - Returns a Double object holding the double value represented by the argument string
+        String doubleStr = "3.14";
+        Double doubleObj = Double.parseDouble(doubleStr);
+        System.out.println("Double value: " + doubleObj);
+
+        // 3. Boolean.valueOf() - Returns a Boolean with a value represented by the specified string
+        String boolStr = "true";
+        Boolean boolObj = Boolean.valueOf(boolStr);
+        System.out.println("Boolean value: " + boolObj);
+
+        // 4. Long.toHexString() - Returns a hexadecimal string representation of the long argument
+        long longValue = 255;
+        String hexString = Long.toHexString(longValue);
+        System.out.println("Hexadecimal representation of " + longValue + ": " + hexString);
+
+        // 5. Character.isDigit() - Determines if the specified character is a digit
+        char ch = '7';
+        boolean isDigit = Character.isDigit(ch);
+        System.out.println("Is " + ch + " a digit? " + isDigit);
     }
 }
